f2c084a3296428a07cd2a5a5000565d9d7e530cb
ramizgit <rramiz.rraza@gmail.com>
2023-03-14 08:04:55 +0530

    Adding gatling programming for performance testing

---
+++ src/test/java/computerdatabase/TatkalcareTestSimulation.java
@@ -0,0 +1,42 @@
+package computerdatabase;
+
+import io.gatling.javaapi.core.ChainBuilder;
+import io.gatling.javaapi.core.ScenarioBuilder;
+import io.gatling.javaapi.core.Simulation;
+import io.gatling.javaapi.http.HttpProtocolBuilder;
+
+import static io.gatling.javaapi.core.CoreDsl.*;
+import static io.gatling.javaapi.http.HttpDsl.http;
+import static io.gatling.javaapi.http.HttpDsl.status;
+
+public class TatkalcareTestSimulation extends Simulation {
+
+    ChainBuilder edit =
+            // let's try at max 2 times
+            tryMax(2)
+                    .on(
+                            exec(
+                                    http("post")
+                                            //.post("/login?mobile=8884592118&sponsor=123456")
+                                            .post("/clinicwisedoctor-for-clinic")
+                                            .header("token", "Bearer 1229|aXeNxqymqffVhK8XZhpAwrmQIqqzX24u9GQYdNlw")
+                                            .check(
+                                            status().in(200, 201)
+                                    )
+                            )
+                    )
+                    // if the chain didn't finally succeed, have the user exit the whole scenario
+                    .exitHereIfFailed();
+
+    HttpProtocolBuilder httpProtocol =
+            http.baseUrl("https://tatkalcares.com/api/v1/clinic");
+
+    ScenarioBuilder admins = scenario("Login").exec(edit);
+
+    {
+        System.out.println("hiiiiiiiiiiiii");
+        setUp(
+                admins.injectOpen(rampUsers(100).during(1))
+        ).protocols(httpProtocol);
+    }
+}
