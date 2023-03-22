e770b568216baf8ff8833c5ac9c636036b8cd593
ramizgit <65121963+ramizgit@users.noreply.github.com>
2023-03-22 17:11:08 +0530

    Update InputParam.java

--- src/test/java/tatkalcare/input/InputParam.java
+++ src/test/java/tatkalcare/input/InputParam.java
@@ -2,9 +2,8 @@ package tatkalcare.input;
 
 public class InputParam {
 
-    public static int TIMES = 1;//20;
+    public static int TIMES = 20;
     public static int PAUSE = 1;
     public static int HTTP_OK=200;
     public static int HTTP_CREATED=201;
-
 }
